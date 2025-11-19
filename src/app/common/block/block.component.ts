2d48194455d52883c7a626067a0824c100e6f809
jawad <jawad@correctcloud.io>
2025-11-19 12:27:11 +0530

    created hovering block

--- src/app/common/block/block.component.ts
+++ src/app/common/block/block.component.ts
@@ -10,13 +10,4 @@ export class BlockComponent {
   @Input() imageUrl: string = '';
   @Input() name: string = '';
   @Input() content: string = '';
-  showPopup: boolean = false;
-
-  openDetailPopup() {
-    this.showPopup = true;
-  }
-
-  closeDetailPopup() {
-    this.showPopup = false;
-  }
 }
